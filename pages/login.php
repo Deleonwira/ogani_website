a0645c819289f639b0692dd949a122f6fbccb259
Fajar Fathurachman <ai.fathur.ai@gmail.com>
2025-11-03 15:20:31 +0700

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -120,10 +120,6 @@
 <script>
 let container = document.getElementById('container')
 
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
 
 setTimeout(() => {
 	container.classList.add('sign-in')
