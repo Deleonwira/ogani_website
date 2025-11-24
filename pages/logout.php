16f8daebeef4c0bec35401e4e4730b24f270c1f2
Fajar Fathurachman <ai.fathur.ai@gmail.com>
2025-11-24 15:45:09 +0700

    perbaikan atau penambahan file yg hilang

---
+++ pages/logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+session_unset();   // Hapus semua variabel session
+session_destroy(); // Hapus session sepenuhnya
+header("Location: ./login.php"); // Arahkan kembali ke halaman login
+exit;
+?>
