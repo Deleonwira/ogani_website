109cab27adbf67e9856576693878e470d96f1020
Deleonwira <hamzadeleonw123@gmail.com>
2025-11-29 20:08:12 +0700

    Konfigurasi file koneksi database dari xampp ke docker

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,12 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root"; 
-$pass = ""; 
-$db   = "ogani_app";
-
-
+$host = "mysql";       // NAMA SERVICE DI DOCKER COMPOSE
+$user = "root";
+$pass = "password";    // password yang kamu set di compose
+$db   = "myapp";   // nama database
 $conn = new mysqli($host, $user, $pass, $db);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
